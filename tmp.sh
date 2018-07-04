277ebf0df1b11a82b89474d48c143017f4389756
Yannick Carissan <yannick.carissan@univ-amu.fr>
2018-07-04 15:09:34 +0200

    remove unused script

--- tmp.sh
+++
@@ -1,6 +0,0 @@
-echo "# atomes_liaison_chimique" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/pnava/atomes_liaison_chimique.git
-git push -u origin master
